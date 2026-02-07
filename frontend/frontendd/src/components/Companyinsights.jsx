37d909ee21a77893e4344a683ac7893a0b2000c4
tchandok30 <tanishachandok@gmail.com>
2026-02-07 22:23:18 +0530

    Added a cool new feature

---
+++ frontend/frontendd/src/components/Companyinsights.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Companyinsights() {
+  return (
+    <div>Company-insights</div>
+  )
+}
+
+export default Companyinsights
---
+++ frontend/frontendd/src/components/Skill-gap.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Skillgap() {
+  return (
+    <div>Skillgap</div>
+  )
+}
+
+export default Skillgap
